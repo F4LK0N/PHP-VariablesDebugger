f5c4454fe369444a26339d3dab7d7861fb820f4f
Otavio B. Soria <otaviosoria@gmail.com>
2019-06-08 10:43:51 -0300

    Application Structure Example

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+
+require "_framework/loader.php";
+
+?><html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <title>Otavio Soria PHP Variables Debugger</title>
+</head>
+<body>
+
+</body>
+</html>
