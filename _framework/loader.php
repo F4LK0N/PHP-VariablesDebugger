de0ce983263f480d138f868d776573129011bb16
Otavio B. Soria <otaviosoria@gmail.com>
2019-06-08 10:51:58 -0300

    Application Structure Example - Loader adjust

--- _framework/loader.php
+++ _framework/loader.php
@@ -1,11 +1,11 @@
 <?php
 
-require "helpers/0-some-helper.php";
-require "helpers/1-another-helper.php";
-require "helpers/2-one_more_helper.php";
+//require "helpers/0-some-helper.php";
+//require "helpers/1-another-helper.php";
+//require "helpers/2-one_more_helper.php";
 require "helpers/os-variables-debugger.php";
 
-require "your_framework_files/0-some_code.php";
-require "your_framework_files/1-another_code.php";
-require "your_framework_files/2-more_code.php";
+//require "your_framework_files/0-some_code.php";
+//require "your_framework_files/1-another_code.php";
+//require "your_framework_files/2-more_code.php";
 
