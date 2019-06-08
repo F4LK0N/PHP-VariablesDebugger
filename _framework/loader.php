33f305e2bb84c3eda96b01ff75722b458483ddb2
Otavio B. Soria <otaviosoria@gmail.com>
2019-06-08 16:16:17 -0300

    Main script file renamed.

--- _framework/loader.php
+++ _framework/loader.php
@@ -19,7 +19,7 @@ $profiling = [
     //require "helpers/0-some-helper.php";
     //require "helpers/1-another-helper.php";
     //require "helpers/2-one_more_helper.php";
-    require "helpers/os-variables-debugger.php";
+    require "helpers/fkn-variables-debugger.php";
 
     //require "your_framework_files/0-some_code.php";
     //require "your_framework_files/1-another_code.php";
