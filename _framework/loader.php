2a43b21da694754d685c4ea46df65f04ff6445f1
Otavio B. Soria <otaviosoria@gmail.com>
2019-06-08 11:54:14 -0300

    Main page, Profiling

--- _framework/loader.php
+++ _framework/loader.php
@@ -1,11 +1,42 @@
 <?php
 
-//require "helpers/0-some-helper.php";
-//require "helpers/1-another-helper.php";
-//require "helpers/2-one_more_helper.php";
-require "helpers/os-variables-debugger.php";
+//Profiling
+$profiling = [
+    //Timing
+    "timing" => microtime(true),
+    //Memory
+    "memory" => memory_get_usage(false),
+    "memory_real" => memory_get_usage(true),
+    //Memory Peak
+    "memory_peak" => memory_get_usage(false),
+    "memory_peak_real" => memory_get_usage(true),
+];
 
-//require "your_framework_files/0-some_code.php";
-//require "your_framework_files/1-another_code.php";
-//require "your_framework_files/2-more_code.php";
+
+
+//Sources
+{
+    //require "helpers/0-some-helper.php";
+    //require "helpers/1-another-helper.php";
+    //require "helpers/2-one_more_helper.php";
+    require "helpers/os-variables-debugger.php";
+
+    //require "your_framework_files/0-some_code.php";
+    //require "your_framework_files/1-another_code.php";
+    //require "your_framework_files/2-more_code.php";
+}
+
+
+
+//Profiling
+$profiling = [
+    //Timing
+    "timing" => (int)((microtime(true) - $profiling['timing'])*1000),
+    //Memory
+    "memory" => memory_get_usage(false) - $profiling['memory'],
+    "memory_real" => memory_get_usage(true) - $profiling['memory_real'],
+    //Memory Peak
+    "memory_peak" => memory_get_peak_usage(false) - $profiling['memory_peak'],
+    "memory_peak_real" => memory_get_peak_usage(true) - $profiling['memory_peak'],
+];
 
--- index.php
+++ index.php
@@ -10,6 +10,69 @@ require "_framework/loader.php";
     <title>Otavio Soria PHP Variables Debugger</title>
 </head>
 <body>
+    <h1>Otavio Soria's Personal PHP Variables Debugger</h1>
+    <p>
+        A dev tool in your hand.
+    </p>
 
+
+    <h2>About</h2>
+    <p>
+        I initially made this debuggers functions as an easy var_dump alternative, to when you can't use var_dump for any reason or when you can format the result in a more human readable way because of servers limitations or any other reason;<br>
+        It's strong points are:
+    </p>
+    <ul>
+        <li>Accepts variables containing <b>nulls</b>, <b>booleans</b>, <b>integers</b>, <b>floats</b>, <b>strings</b>, <b>arrays</b>, <b>objects</b> and <b>resources</b>.</li>
+        <li>HTML structured results.</li>
+        <li>Customizable layout focused in improve reading.</li>
+        <li>Small and easy to type functions name's.</li>
+        <li>No external dependencies.</li>
+    </ul>
+
+
+    <h2>Installation</h2>
+    <p>
+
+    </p>
+
+
+    <h2>Usage</h2>
+    <p>
+
+    </p>
+
+
+    <h2>Profiling</h2>
+    <p>
+        The script profiling are:
+    </p>
+    <table>
+        <tr>
+            <td><?= $profiling['timing']; ?></td>
+            <td>milliseconds to load.</td>
+        </tr>
+        <tr>
+            <td><?= $profiling['memory']; ?></td>
+            <td>bytes of memory used.</td>
+        </tr>
+        <tr>
+            <td><?= $profiling['memory_real']; ?></td>
+            <td>bytes of real memory used.</td>
+        </tr>
+        <tr>
+            <td><?= $profiling['memory_peak']; ?></td>
+            <td>bytes of memory used on peak.</td>
+        </tr>
+        <tr>
+            <td><?= $profiling['memory_peak_real']; ?></td>
+            <td>bytes of real memory used on peak.</td>
+        </tr>
+    </table>
+
+
+    <h2>Examples</h2>
+    <p>
+
+    </p>
 </body>
 </html>
